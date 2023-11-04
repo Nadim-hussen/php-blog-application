d51f2bf312f97519f1f9510c3fdd677ed32a063d
Nadim <hussennadim49@gmail.com>
2023-11-04 15:55:05 +0600

    Explanation of OOP

--- basicOfOPP.php
+++ basicOfOPP.php
@@ -90,3 +90,154 @@ class Person_1 extends Person{
 }
 $person1 = new Person_1();
 $person1->showEmployee('Nadim','17','Male',5000,'web dev');
+
+// ----------------------------------------------------** 5.Abstraction **-----------------------------------------
+// Abstract classes and methods are when the parent class has a named method, but need its child class(es) to fill out the tasks.
+// An abstract class is a class that contains at least one abstract method. An abstract method is a method that is declared
+// , but not implemented in the code.
+
+// note :: Data Abstraction means any representation of data in which the implementation details are hidden or abstracted.
+abstract class NewPerson {
+    public $name;
+    public $age;
+    function __construct($n,$a){
+        $this->name = $n;
+        $this->age = $a;
+    }
+    public function hello(){
+        echo "This is an abstract class. <br />";
+    }
+    abstract public function showData();
+    abstract public function add($x,$y);
+}
+class AnotherPerson extends NewPerson{
+    public function showData(){
+        echo "My name is ".$this->name.'and I\'m'.$this->age.'year old.<br />';
+    }
+    public function add($x,$y){
+        return $x + $y.'<br />';
+    }
+}
+$newperson = new AnotherPerson('Nadim',24);
+$newperson->showData();
+$newperson->hello();
+echo $newperson->add(10,20);
+
+// --------------------------------------------------** 6.Interface **-----------------------------------------
+// Interfaces allow you to specify what methods a class should implement.
+// Interfaces make it easy to use a variety of different classes in the same way.
+// When one or more classes use the same interface, it is referred to as "polymorphism".
+// Note :: Interfaces are declared with the interface keyword
+// 1 ::
+interface Test1{
+    const A = 10;
+    public function showValue();
+}
+interface Test2{
+    const B = 200;
+    public function showValue();
+}
+class Main implements Test1,Test2{
+    public function showValue(){
+        echo Test2::B.'<br />';
+        echo Test1::A.'<br />';
+    }
+}
+
+$test1 = new Main();
+$test1->showValue();
+// --------------------
+// 2 ::
+interface Login {
+    const Email = 'Nadim@gmail.com';
+    const Password = 123456;
+    public function getEmail();
+    public function getPassword();
+}
+interface Register extends Login{
+    const Phone = '01726603587';
+    public function getPhone();
+}
+
+Class NewUser implements Register {
+    public function getEmail(){
+        echo Register::Email.'<br />';
+    }
+    public function getPassword(){
+        echo Register::Password.'<br />';
+    }
+    public function getPhone(){
+        echo Register::Phone.'<br />';
+    }
+}
+$newUser = new NewUser();
+$newUser->getEmail();
+
+
+// *** different between Abstract and interface :::
+    // 1.Interface supports multiple inheritence. Abstract class doesn't support multiple inheritence
+    // 2.Interface doesn't contain member variable (var $price; var $title;) and constructor. but Abstract contain them.
+    // 3. All interface methods must be public, while abstract class methods is public or protected
+
+// ----------------------------------------------------** 7.Polymorphism **--------------------------------------------
+// Polymorphism in PHP refers to the ability of objects to take on different forms and exhibit different behaviors
+// while sharing a common interface. It allows different classes to implement the same method name with different functionality.
+interface Shape{
+    public function calculateArea();
+}
+class Circle implements Shape{
+    private $radius;
+    public function __construct($r){
+        $this->radius = $r;
+    }
+    public function calculateArea(){
+        echo "Area of circle= ". pi()*$this->radius*$this->radius.'<br />';
+    }
+}
+class Rectangle implements Shape {
+    private $height;
+    private $width;
+    public function __construct($h,$w){
+        $this->height = $h;
+        $this->width = $w;
+    }
+    public function calculateArea(){
+        echo "Area of triangle= ".$this->height * $this->width.'<br />';
+    }
+}
+class Triangle implements Shape {
+    private $base;
+    private $height;
+    public function __construct($b,$h){
+        $this->base = $b;
+        $this->height = $h;
+    }
+    public function calculateArea(){
+        echo "Area of triangle = ". ($this->base * $this->height)/2 ."<br />";
+    }
+}
+
+$circle = new Circle(5);
+$rect = new Rectangle(10,30);
+$tri = new Triangle(10,30);
+$circle->calculateArea();
+$rect->calculateArea();
+$tri->calculateArea();
+
+
+// ---------------------------------- **overriding** ---------------------------------------
+
+class Base {
+    public function hello(){
+        echo "This is a base class <br />";
+    }
+}
+class Child extends Base {
+    public function hello(){
+        echo "This is child class <br />";
+    }
+}
+$b = new Base;
+$c = new Child;
+$b->hello();
+$c->hello(); // hello method changes in Child.
